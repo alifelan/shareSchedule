2f336355aba85db134f013847dda017e0affc829
Ali <alifelanv@gmail.com>
2018-12-11 04:44:37 -0600

    Changed path

--- shareSchedule/urls.py
+++ shareSchedule/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('schedules/', include('schedules.urls')),
+    path('', include('schedules.urls')),
 ]
